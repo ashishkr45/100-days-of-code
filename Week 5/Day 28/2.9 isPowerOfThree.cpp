1e8d021b4e434b4182812dc1d11f6e40a3a4b4d3
ashishkr45 <149966759+ashishkr45@users.noreply.github.com>
2024-07-24 20:30:51 +0530

    commit

---
+++ Week 5/Day 28/2.9 isPowerOfThree.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+bool isPowerOfThree(int n)
+{
+    if (n == 0)
+        return true;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    isPowerOfThree(n);
+
+    return 0;
+}
