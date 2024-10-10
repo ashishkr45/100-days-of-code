37322472d37d49579b82bc50e5e517ef822919e0
ashishkr45 <149966759+ashishkr45@users.noreply.github.com>
2024-10-10 17:50:38 +0530

    commit

---
+++ Week 16/Day 5/36.08 level_order.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+class Tree
+{
+private:
+    static int idx;
+
+public:
+    Tree()
+    {
+        idx = -1;
+    }
+
+    Node *buildTree(vector<int> nodes)
+    {
+        idx++;
+
+        if (nodes[idx] == -1)
+        {
+            return NULL;
+        }
+        Node *currNode = new Node(nodes[idx]);
+        currNode->left = buildTree(nodes);
+        currNode->right = buildTree(nodes);
+
+        return currNode;
+    }
+
+    void preOrder(Node *root)
+    {
+        if (root == NULL)
+            return;
+        cout << root->data << " ";
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+
+    void inOrder(Node *root)
+    {
+        if (root == NULL)
+            return;
+        inOrder(root->left);
+        cout << root->data << " ";
+        inOrder(root->right);
+    }
+
+    void postOrder(Node *root)
+    {
+        if (root == NULL)
+            return;
+        postOrder(root->left);
+        postOrder(root->right);
+        cout << root->data << " ";
+    }
+
+    void levelOrder(Node *root)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+        queue<Node *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            Node *curr = q.front();
+            q.pop();
+            cout << curr->data << " ";
+
+            if (curr->left != NULL)
+            {
+                q.push(curr->left);
+            }
+
+            if (curr->right != NULL)
+            {
+                q.push(curr->right);
+            }
+        }
+        cout << endl;
+    }
+};
+
+int Tree::idx = -1;
+
+int main()
+{
+    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    Tree t;
+
+    Node *root = t.buildTree(nodes);
+
+    cout << "Root: " << root->data << endl;
+
+    cout << "Pre-Order: ";
+    t.preOrder(root);
+    cout << endl;
+
+    cout << "In-order: ";
+    t.inOrder(root);
+    cout << endl;
+
+    cout << "Post-Order: ";
+    t.postOrder(root);
+    cout << endl;
+
+    cout << "Level Order: ";
+    t.levelOrder(root);
+    cout << endl;
+
+    return 0;
+}
