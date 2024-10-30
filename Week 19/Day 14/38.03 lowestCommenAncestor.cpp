02beda04f4a16f489886964b7ddb85c8113062a5
-lerner <ashishkr@gmail.com>
2024-10-30 10:30:35 +0530

    added comments in 38.03

--- Week 19/Day 14/38.03 lowestCommenAncestor.cpp
+++ Week 19/Day 14/38.03 lowestCommenAncestor.cpp
@@ -34,35 +34,35 @@ bool CommenAncestorPath(Node* root, int n, vector<int> &path){
 		 return false;
 	}
 
-	path.push_back(root->data); 
+	path.push_back(root->data); //we rooting that this is the path to the node we looking
 
-	if(root->data == n){
+	if(root->data == n){ //at any level we geting the data retuen true;
 		return true;
 	}
 
-	bool isLeft = CommenAncestorPath(root->left, n, path);
-	bool isright = CommenAncestorPath(root->right, n, path);
+	bool isLeft = CommenAncestorPath(root->left, n, path); //checking for the left subtrees
+	bool isright = CommenAncestorPath(root->right, n, path); //checking for the right subtrees
 
-	if(isLeft || isright){
+	if(isLeft || isright){ // if we got The Node in any of the subTree
 		return true;
 	}
 
-	path.pop_back();
+	path.pop_back(); // else return false and pop the elements
 	return false;
 }
 
 int LCA(Node* root, int n1, int n2){
-	vector<int> path1;
-	vector<int> path2;
+	vector<int> path1; // making two vec to store the path till n1
+	vector<int> path2; // making two vec to store the path till n2
 
-	CommenAncestorPath(root, n1, path1);
+	CommenAncestorPath(root, n1, path1); //we'll be getting the paths to n1 & n2
 	CommenAncestorPath(root, n2, path2);
 	
 	int lca = -1;
 	for (int i = 0, j = 0; i <(int) path1.size() && j <(int) path2.size(); i++, j++){
 		if(path1[i] != path2[j]) return lca;
 
-		lca = path1[i];
+		lca = path1[i]; //if noting is same the root will be
 	}
 	
 }
