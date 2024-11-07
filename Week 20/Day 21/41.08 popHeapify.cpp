2f54eacb0a8340bb32c1dfb05665ac969910469d
-lerner <ashishkr@gmail.com>
2024-11-07 18:14:25 +0530

    implemend pop and heapify

--- Week 20/Day 21/41.08 popHeapify.cpp
+++ Week 20/Day 21/41.08 popHeapify.cpp
@@ -4,7 +4,34 @@
 using namespace std;
 
 class Heap {
+private:
 	vector<int> vec;
+
+	void heapify(int i) { // making this function private
+	/*We dont want it's asscess outside the class*/
+		if(i >= (int)vec.size()) {
+			return;
+		}
+
+		int left = 2*i + 1;
+		int right = 2*i + 2;
+
+		int maxI = i;
+
+		if(left <(int) vec.size() && vec[left] > vec[maxI]) {
+			maxI = left;
+		}
+
+		if(right <(int) vec.size() && vec[right] > vec[maxI]) {
+			maxI = right;
+		}
+
+		swap(vec[i], vec[maxI]);
+		if(maxI != i) {
+			heapify(maxI);
+		}
+	}
+
 public:
 	void push (int data) {
 		vec.push_back(data);
@@ -21,7 +48,14 @@ public:
 	}
 
 	void pop() {
+		//step 1: swap root(larges Idx and last elem)
+		swap(vec[0], vec[vec.size() - 1]);
 
+		//step 2: delete the last idx
+		vec.pop_back();
+
+		//step 3: call heapify for the root idx
+		heapify(0);
 	}
 
 	int top() {
@@ -29,7 +63,7 @@ public:
 	}
 
 	bool empty() {
-		return vec.empty() == 0;
+		return vec.empty();
 	}
 };
 
@@ -41,7 +75,10 @@ int main() {
 	heap.push(69);
 	heap.push(30);
 
-	cout<<heap.top()<<endl;
-
+	while (!heap.empty()) {
+		cout << "Top: "<< heap.top() << endl; 
+		heap.pop();
+	}
+	
 	return 0;
 }
